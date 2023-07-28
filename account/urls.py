b6c78a63101ffd6b99bc31faa31f72f02c017fbc
JM Bae <jungman82@gmail.com>
2023-07-28 14:29:13 +0900

    로그아웃 GET 방식 호출 함수 추가로 해결

--- account/urls.py
+++ account/urls.py
@@ -8,6 +8,6 @@ app_name = 'account'
 urlpatterns = [
     # path('login/', views.user_login, name='login')
     path('login/', auth_views.LoginView.as_view(), name='login'),
-    path('logout/', auth_views.LogoutView.as_view(), name='logout'),
+    path('logout/', views.user_logout, name='logout'),
     path('', views.dashboard, name='dashboard'),
 ]
--- account/views.py
+++ account/views.py
@@ -1,4 +1,4 @@
-from django.contrib.auth import authenticate, login
+from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import login_required
 from django.http import HttpResponse
 from django.shortcuts import render
@@ -30,3 +30,8 @@ def user_login(request):
 @login_required
 def dashboard(request):
     return render(request, 'account/dashboard.html', {'section': 'dashboard'})
+
+
+def user_logout(request):
+    logout(request)
+    return render(request, 'account/logged_out.html')
