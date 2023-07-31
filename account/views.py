d445d6fe2613dc097d768aeefe3c1a2f57719847
JM Bae <jungman82@gmail.com>
2023-07-31 14:37:07 +0900

    유저 수정 폼, 프로필 수정 폼 추가 회원가입시 프로필 모델 생성 코드 추가

--- account/views.py
+++ account/views.py
@@ -25,6 +25,8 @@ def register(request):
             new_user = user_form.save(commit=False)
             new_user.set_password(user_form.cleaned_data['password'])
             new_user.save()
+            from account.models import Profile
+            Profile.objects.create(user=new_user)
             return render(request,
                           'account/register_done.html',
                           {'new_user': new_user})
