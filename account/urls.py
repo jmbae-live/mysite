848e0ad9574606d3482ac74878954d4c1ce5820a
JM Bae <jungman82@gmail.com>
2023-07-31 13:44:07 +0900

    이미 로그인한 사용자가 로그인 페이지 접근시 대시보드로 리다이렉트

--- account/urls.py
+++ account/urls.py
@@ -2,8 +2,10 @@ from django.urls import path, include
 from django.contrib.auth import views as auth_views
 
 from account import views
+from account.views import CustomLoginView
 
 urlpatterns = [
+    path('login/', CustomLoginView.as_view(), name='login'),
     path('', include('django.contrib.auth.urls')),
     path('', views.dashboard, name='dashboard'),
     path('register/', views.register, name='register'),
