129ca9b8f584d697cdfd903ffcd7f6704f889aec
JM Bae <jungman82@gmail.com>
2023-08-01 14:24:20 +0900

    소셜 인증 프로필 추가 로직, 소셜 로그인 파이프라인 수정

--- mysite/settings.py
+++ mysite/settings.py
@@ -164,3 +164,16 @@ SOCIAL_AUTH_FACEBOOK_KEY = '852581883051667'
 SOCIAL_AUTH_FACEBOOK_SECRET = 'e5b65a26fce6189df9c62c23bb85ae20'
 
 SOCIAL_AUTH_FACEBOOK_SCOPE = ['email']
+
+SOCIAL_AUTH_PIPELINE = [
+    'social_core.pipeline.social_auth.social_details',
+    'social_core.pipeline.social_auth.social_uid',
+    'social_core.pipeline.social_auth.auth_allowed',
+    'social_core.pipeline.social_auth.social_user',
+    'social_core.pipeline.user.get_username',
+    'social_core.pipeline.user.create_user',
+    'account.authentication.create_profile',
+    'social_core.pipeline.social_auth.associate_user',
+    'social_core.pipeline.social_auth.load_extra_data',
+    'social_core.pipeline.user.user_details',
+]
