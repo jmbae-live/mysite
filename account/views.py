e83fd62410561d612697cc6a00acaaad3fc91808
JM Bae <jungman82@gmail.com>
2023-08-03 15:33:49 +0900

    유저 팔로우 뷰함수, 자바스크립트 코드 추가

--- account/views.py
+++ account/views.py
@@ -2,9 +2,11 @@ from django.contrib import messages
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.models import User
 from django.contrib.auth.views import LoginView
+from django.http import JsonResponse
 from django.shortcuts import render, redirect, get_object_or_404
+from django.views.decorators.http import require_POST
 
-from account.models import Profile
+from account.models import Profile, Contact
 from .forms import UserRegistrationForm, UserEditForm, ProfileEditForm
 
 
@@ -77,3 +79,23 @@ def user_detail(request, username):
     return render(request,
                   'account/user/detail.html',
                   {'section': 'people', 'user': user})
+
+
+@require_POST
+@login_required
+def user_follow(request):
+    user_id = request.POST.get('id')
+    action = request.POST.get('action')
+    if user_id and action:
+        try:
+            user = User.objects.get(id=user_id)
+            if action == 'follow':
+                Contact.objects.get_or_create(
+                    user_from=request.user,
+                    user_to=user)
+            else:
+                Contact.objects.filter(user_from=request.user, user_to=user).delete()
+            return JsonResponse({'status': 'ok'})
+        except User.DoesNotExist:
+            return JsonResponse({'status': 'error'})
+    return JsonResponse({'status': 'error'})
