2c1748964401933f7c901f0faa114cbeac5746aa
JM Bae <jungman82@gmail.com>
2023-08-07 11:49:44 +0900

    redis views 조회 카운트 기능 추가

--- images/views.py
+++ images/views.py
@@ -1,3 +1,4 @@
+import redis
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
@@ -8,6 +9,11 @@ from django.views.decorators.http import require_POST
 from actions.utils import create_action
 from images.forms import ImageCreateForm
 from images.models import Image
+from mysite import settings
+
+r = redis.Redis(host=settings.REDIS_HOST,
+                port=settings.REDIS_PORT,
+                db=settings.REDIS_DB)
 
 
 # Create your views here.
@@ -32,9 +38,12 @@ def image_create(request):
 
 def image_detail(request, id, slug):
     image = get_object_or_404(Image, id=id, slug=slug)
+    total_views = r.incr(f"image:{image.id}:views")
     return render(request,
                   'images/image/detail.html',
-                  {'section': 'images', 'image': image})
+                  {'section': 'images',
+                   'image': image,
+                   'total_views': total_views})
 
 
 @login_required
