61beccd1d64f61b4280e1f3e494acf92e200b8b7
JM Bae <jungman82@gmail.com>
2023-08-08 15:06:23 +0900

    카트 뷰 함수, URL 추가

--- mysite/urls.py
+++ mysite/urls.py
@@ -34,6 +34,7 @@ urlpatterns = [
     path('social-auth/', include('social_django.urls', namespace='social')),
     path('images/', include('images.urls', namespace='images')),
     path('__debug__/', include('debug_toolbar.urls')),
+    path('cart/', include('cart.urls', namespace='cart')),
     path('', include('shop.urls', namespace='shop')),
 ]
 
