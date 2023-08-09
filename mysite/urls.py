619ede33b2083cca2af5712bc1a00c04ccbfa04f
JM Bae <jungman82@gmail.com>
2023-08-09 15:12:58 +0900

    주문 생성 뷰 함수 추가, URL 경로 추가

--- mysite/urls.py
+++ mysite/urls.py
@@ -34,6 +34,7 @@ urlpatterns = [
     path('social-auth/', include('social_django.urls', namespace='social')),
     path('images/', include('images.urls', namespace='images')),
     path('cart/', include('cart.urls', namespace='cart')),
+    path('orders/', include('orders.urls', namespace='orders')),
     path('__debug__/', include('debug_toolbar.urls')),
     path('', include('shop.urls', namespace='shop')),
 ]
