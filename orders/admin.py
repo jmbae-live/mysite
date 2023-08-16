e2f81cfba4f3708461b28cd44126c20bdc0995a1
JM Bae <jungman82@gmail.com>
2023-08-16 11:34:39 +0900

    관리자 사이트 결제 참조 url 커스텀 필드 추가

--- orders/admin.py
+++ orders/admin.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
+from django.utils.safestring import mark_safe
 
 from orders.models import Order, OrderItem
 
@@ -9,10 +10,17 @@ class OrderItemInline(admin.TabularInline):
     raw_id_fields = ['product']
 
 
+def order_payment(obj):
+    if obj.stripe_id:
+        html = f'<a href="{obj.get_stripe_url()}" target="_blank">{obj.stripe_id}</a>'
+        return mark_safe(html)
+    return ''
+
+
 @admin.register(Order)
 class OrderAdmin(admin.ModelAdmin):
     list_display = ['id', 'first_name', 'last_name', 'email',
                     'address', 'postal_code', 'city', 'paid',
-                    'created', 'updated']
+                    order_payment, 'created', 'updated']
     list_filter = ['paid', 'created', 'updated']
     inlines = [OrderItemInline]
