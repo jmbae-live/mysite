696cf9be94eb86c52c1b3c1cf390b35d0636409a
JM Bae <jungman82@gmail.com>
2023-08-22 10:22:50 +0900

    url 패턴 국제화

--- mysite/urls.py
+++ mysite/urls.py
@@ -15,32 +15,40 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.conf import settings
+from django.conf.urls.i18n import i18n_patterns
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.contrib.sitemaps.views import sitemap
 from django.urls import path, include
+from django.utils.translation import gettext_lazy as _
 
 from blog.sitemaps import PostSitemap
+from payment import webhooks
 
 sitemaps = {
     'posts': PostSitemap,
 }
 
-urlpatterns = [
+urlpatterns = i18n_patterns(
     path("admin/", admin.site.urls),
     path("account/", include('account.urls')),
     path("blog/", include('blog.urls', namespace='blog')),
     path("sitemap.xml", sitemap, {'sitemaps': sitemaps}),
     path('social-auth/', include('social_django.urls', namespace='social')),
     path('images/', include('images.urls', namespace='images')),
-    path('cart/', include('cart.urls', namespace='cart')),
-    path('orders/', include('orders.urls', namespace='orders')),
-    path('payment/', include('payment.urls', namespace='payment')),
-    path('coupons/', include('coupons.urls', namespace='coupons')),
+    path(_('cart/'), include('cart.urls', namespace='cart')),
+    path(_('orders/'), include('orders.urls', namespace='orders')),
+    path(_('payment/'), include('payment.urls', namespace='payment')),
+    path(_('coupons/'), include('coupons.urls', namespace='coupons')),
     path('rosetta/', include('rosetta.urls')),
     path('__debug__/', include('debug_toolbar.urls')),
     path('', include('shop.urls', namespace='shop')),
+)
+
+urlpatterns += [
+    path('payment/webhook/', webhooks.stripe_webhook, name='stripe-webhook'),
 ]
+
 
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL,
